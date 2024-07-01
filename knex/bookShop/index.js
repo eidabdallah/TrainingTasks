38170a1c2d00ff16dab4f9d6795396885fb4c578
eidabdallah <eidabdallah971@gmail.com>
2024-07-01 13:09:04 +0300

    update index file

--- knex/bookShop/index.js
+++ knex/bookShop/index.js
@@ -9,11 +9,18 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.get('/', (req, res) => {
+    return res.json({ message: "welcome" });
+});
+
 app.use('/book', bookRoute);
 app.use('/author', authorRoute);
 app.use('/publisher', publisherRoute);
 app.use('/reserve', reserveRoute);
 
+app.use('*', (req, res) => {
+    return res.json({ message: "page not found" });
+});
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
